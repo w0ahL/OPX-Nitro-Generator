490a8bada22d6dc576e995c6e3e1adae4f72a704
w0ahL <149548505+w0ahL@users.noreply.github.com>
2023-12-24 19:27:02 +0000

    Upload files /w 490 generated codes

---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require('fs/promises');
+
+const url = 'https://api.discord.gx.games/v1/direct-fulfillment';
+const outputFile = 'tokens.txt';
+
+const config = require("./config.json");
+
+setInterval(() => {
+  (async () => {
+    const data = {
+      partnerUserId: "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g,
+        (function(t) {
+             const e = 16 * Math.random() | 0;
+            return ("x" === t ? e : 3 & e | 8).toString(16)
+        })),
+    };
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      if (response.ok) {
+        const responseData = await response.json();
+
+       const tokenLink = `https://discord.com/billing/partner-promotions/1180231712274387115/${responseData.token}`;
+
+        console.log(`(+) ${tokenLink}`);
+
+        await fs.appendFile(outputFile, tokenLink + '\n', 'utf-8');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  })();
+}, config.cooldown)
